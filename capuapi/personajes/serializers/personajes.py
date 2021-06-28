2bcbc8f435334cfee4a4b0b630836127c78d9c39
Juan Cruz <juancruz.romero.dev@gmail.com>
2021-06-28 00:17:25 -0300

    Update Personajes serializers

--- capuapi/personajes/serializers/personajes.py
+++ capuapi/personajes/serializers/personajes.py
@@ -8,11 +8,13 @@ class PersonajeSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     name = serializers.CharField()
     about = serializers.CharField()
+    video_url = serializers.CharField()
 
 class CreatePersonajeSerializer(serializers.Serializer):
     name = serializers.CharField()
     about = serializers.CharField()
-    
+    video_url = serializers.CharField()
+
     def create(self, data):
         """Create personaje."""
         return Personaje.objects.create(**data)
