bf5939eaa551e6c1049e3b2a9e67fe471c610f67
Juan Cruz <juancruz.romero.dev@gmail.com>
2021-06-28 00:16:26 -0300

    Update Personajes model

--- capuapi/personajes/models/personajes.py
+++ capuapi/personajes/models/personajes.py
@@ -1,14 +1,22 @@
 # Django
 from django.db import models
-
 # Utilities
 from capuapi.utils.models import CapuAPIModel
 
 class Personaje(CapuAPIModel):
-    """ TODO: Documentar...
+    """Personaje Model. Indico la composición del modelo
+    de personajes.
+
+    Args:
+        CapuAPIModel (obj): Herencia de datos básicos para
+        completar el model.
+
+    Returns:
+        str: Retorna el nombre.
     """
     name = models.CharField('nombre', max_length=140)
-    about = models.CharField('descripcion personaje', max_length=255)
+    about = models.CharField('acerca del personaje', max_length=255)
+    video_url = models.CharField('video de presentación', max_length=255)
 
     def __str__(self):
         """Return circle name."""
